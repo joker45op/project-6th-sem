7e2e0b0df25d18db46cc6f294b4f404eea1c2cca
joker45op <85115298+joker45op@users.noreply.github.com>
2023-03-16 12:05:19 +0530

    start

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
 // document.querySelector('#app').innerHTML = `
-
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `
